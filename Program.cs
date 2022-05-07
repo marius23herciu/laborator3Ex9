8d692f3c2aebec86a29d141e5f6f17a8816fa627
sebastian popescu <sebastianh123@yahoo.com>
2022-05-07 11:36:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace laborator3Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti un program care citind de la tastatura lungimea si latimea unui
+            //dreptungh, va afisa diagonala acestuia.
+            //             Calculul diagonalei va fi extras intr-o functie
+
+            Console.WriteLine("Introduceti lungimea:");
+            int lungime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti latimea:");
+            int latime = int.Parse(Console.ReadLine());
+
+            DiagonalaDreptunghi(lungime, latime);
+        }
+        static void DiagonalaDreptunghi(int lungime, int latime)
+        {
+            if (lungime<1||latime<1)
+            {
+                Console.WriteLine("Introduceti numere pozitive.");
+                return;
+            }
+
+            double diagonala = Math.Sqrt(Math.Pow(lungime, 2) + Math.Pow(latime, 2)) ;
+            Console.WriteLine("Diagonala dreptunghiului este " + diagonala);
+        }
+    }
+}
